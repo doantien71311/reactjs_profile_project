e9f808864a0f0ebcc79bb290c50154f2882d199c
bui doan tien <mrtien71311@gmail.com>
2025-07-04 20:32:50 +0700

    4

--- vite.config.app_profile.ts
+++ vite.config.app_profile.ts
@@ -5,12 +5,6 @@ import { resolve } from "path";
 export default defineConfig({
   plugins: [react()],
   //
-  // resolve: {
-  //   alias: {
-  //     "@app": resolve(__dirname, "src/app_profile"),
-  //   },
-  // },
-
   //Tiến thêm vào để giảu quyết quill
   build: {
     commonjsOptions: {
@@ -22,11 +16,6 @@ export default defineConfig({
       //   resolve(__dirname, "src/app_profile/main.jsx"),
       //   "./index_profile.html",
       // ],
-      input: {
-        index: "./index_profile.html",
-        // main: resolve(__dirname, "src/mainProfile.tsx"),
-        //   // app_profile: resolve(__dirname, "src/mainProfile.jsx"), // Example entry for app1
-      },
       output: {
         // Output configuration for app1
         dir: resolve(__dirname, "dist_app_profile"),
