4634a162964d908667cd5b65d2c3a56a5e1000ff
bui doan tien <mrtien71311@gmail.com>
2025-07-04 20:10:47 +0700

    comm itensdf sdfsdfsdfsdfsdfsf

--- vite.config.app_profile.ts
+++ vite.config.app_profile.ts
@@ -18,15 +18,15 @@ export default defineConfig({
     },
 
     rollupOptions: {
-      input: [
-        resolve(__dirname, "src/app_profile/main.jsx"),
-        "./index_profile.html",
-      ],
-      // input: {
-      //   index: "./index.html",
-      //   main: resolve(__dirname, "src/mainProfile.jsx"),
-      //   // app_profile: resolve(__dirname, "src/mainProfile.jsx"), // Example entry for app1
-      // },
+      // input: [
+      //   resolve(__dirname, "src/app_profile/main.jsx"),
+      //   "./index_profile.html",
+      // ],
+      input: {
+        index: "./index.html",
+        //   main: resolve(__dirname, "src/mainProfile.jsx"),
+        //   // app_profile: resolve(__dirname, "src/mainProfile.jsx"), // Example entry for app1
+      },
       output: {
         // Output configuration for app1
         dir: resolve(__dirname, "dist_app_profile"),
